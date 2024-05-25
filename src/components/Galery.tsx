d72b57e982a986d6c705d3a2b72e0109c5a66634
split73 <54321lexa12345@mail.ru>
2024-05-25 14:39:35 +0400

    pagination - 90% responsivnes - 90%

--- src/components/Galery.tsx
+++ src/components/Galery.tsx
@@ -6,6 +6,7 @@ import Header from "./Header";
 import filterIcon from "../assets/filter_icon.svg";
 import searchIcon from "../assets/search_icon.svg";
 import smallCloseIcon from "../assets/small_close_icon.svg";
+import paginationArrowIcon from "../assets/pagination_arrow_icon.svg";
 export type CardType = {
   authorId: number;
   created: string;
@@ -67,14 +68,35 @@ const Galery = () => {
     },
   ]);
   const [currentPage, setCurrentPage] = useState<number>(1);
-  const [lastPage, setLastPage] = useState<number>(5);
+  const [lastPage, setLastPage] = useState<number>(9);
   const [imagesPerPage, setImagesPerPage] = useState<number>(1);
   const [pagination, setPagination] = useState<JSX.Element[]>([]);
+  const [searchInput, setSearchInput] = useState("" )
   const BaseUrl = "https://test-front.framework.team";
-
+  
   const handleChangePage = (page: number) => {
-    console.log(page);
+    setCurrentPage(page);
   };
+
+  const handleDeacreaseCurrentPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage((prevCurrentPage) => prevCurrentPage - 1);
+    }
+  };
+
+  const handleIncreaseCurrentPage = () => {
+    if (currentPage < lastPage) {
+      setCurrentPage((prevCurrentPage) => prevCurrentPage + 1);
+    }
+  };
+
+  const hadnleSearchInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const filteredCardData = cardData.filter((el) => (
+      el.name.toLowerCase().includes(e.target.value.toLowerCase())
+    ))
+    setCardData(filteredCardData)
+    console.log(e.target.value)
+  }
 
   useEffect(() => {
     // axios.get(`${BaseUrl}/paintings?_page=1&_limit=6`).then((res) => {
@@ -82,41 +104,117 @@ const Galery = () => {
     //   setCardData(res.data);
     // });
 
+    function addPageToPagination(page: number): void {
+      if (page === currentPage) {
+        setPagination((prevState) => [
+          ...prevState,
+          <li
+            className="paginationButton"
+            id="activePaginationButton"
+            onClick={() => handleChangePage(page)}
+          >
+            {page}
+          </li>,
+        ]);
+      } else {
+        setPagination((prevState) => [
+          ...prevState,
+          <li
+            className="paginationButton"
+            onClick={() => handleChangePage(page)}
+          >
+            {page}
+          </li>,
+        ]);
+      }
+    }
+
     function paginate() {
+      setPagination([]);
       if (lastPage <= 5) {
         for (let i = 1; i <= lastPage; i++) {
-          if (i === currentPage) {
-            setPagination((prevState) => [
-              ...prevState,
-              <li
-                className="page-item active"
-                onClick={() => handleChangePage(i)}
-              >
-                {i}
-              </li>,
-            ]);
-          } else {
-            setPagination((prevState) => [
-              ...prevState,
-              <li className="page-item" onClick={() => handleChangePage(i)}>
-                {i}
-              </li>,
-            ]);
+          addPageToPagination(i);
+        }
+      } else {
+        if (currentPage < 4) {
+          for (let i = 1; i <= 4; i++) {
+            addPageToPagination(i);
           }
+          setPagination((prevState) => [
+            ...prevState,
+            <li className="paginationButton excessPage">...</li>,
+          ]);
+          setPagination((prevState) => [
+            ...prevState,
+            <li
+              className="paginationButton"
+              onClick={() => handleChangePage(lastPage)}
+            >
+              {lastPage}
+            </li>,
+          ]);
+        } else if (currentPage >= lastPage - 2) {
+          setPagination((prevState) => [
+            ...prevState,
+            <li
+              className="paginationButton"
+              onClick={() => handleChangePage(1)}
+            >
+              {1}
+            </li>,
+          ]);
+          setPagination((prevState) => [
+            ...prevState,
+            <li className="paginationButton excessPage">...</li>,
+          ]);
+          for (let i = lastPage - 3; i <= lastPage; i++) {
+            addPageToPagination(i);
+          }
+        } else {
+          setPagination((prevState) => [
+            ...prevState,
+            <li
+              className="paginationButton"
+              onClick={() => handleChangePage(1)}
+            >
+              {1}
+            </li>,
+          ]);
+          setPagination((prevState) => [
+            ...prevState,
+            <li className="paginationButton excessPage">...</li>,
+          ]);
+          for (let i = currentPage - 1; i <= currentPage + 1; i++) {
+            addPageToPagination(i);
+          }
+          setPagination((prevState) => [
+            ...prevState,
+            <li className="paginationButton excessPage">...</li>,
+          ]);
+          setPagination((prevState) => [
+            ...prevState,
+            <li
+              className="paginationButton"
+              onClick={() => handleChangePage(lastPage)}
+            >
+              {lastPage}
+            </li>,
+          ]);
         }
       }
     }
 
     paginate();
-  }, []);
+  }, [currentPage]);
 
   return (
-    <div>
+    <div style={{ backgroundColor: "#121212"}}>
+      
+      <div id="galleryWrapper" >
       <Header />
-      <div id="galleryWrapper">
         <label id="searchBar">
           <img id="searchIcon" src={searchIcon}></img>
-          <input placeholder="Placeholder"></input>
+          <input placeholder="Placeholder" onChange={(e) => hadnleSearchInput(e)}></input>
           <img id="smallCloseIcon" src={smallCloseIcon}></img>
         </label>
         <button id="filterButton">
@@ -129,7 +227,21 @@ const Galery = () => {
           </ul>
         </main>
         <div id="paginationWrapper">
-          <ul>{pagination}</ul>
+          <ul id="paginationList">
+            <li
+              className="paginationButton"
+              onClick={handleDeacreaseCurrentPage}
+            >
+              <img id="paginationLeftArrow" src={paginationArrowIcon}></img>
+            </li>
+            {pagination}
+            <li
+              className="paginationButton"
+              onClick={handleIncreaseCurrentPage}
+            >
+              <img src={paginationArrowIcon} />
+            </li>
+          </ul>
         </div>
       </div>
     </div>
